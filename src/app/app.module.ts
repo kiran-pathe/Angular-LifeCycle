2e6e112cd43daa7a8099ceaba9c98eeab99bfbaf
kiran-pathe <pathe.kiran@gmail.com>
2020-08-23 14:59:28 +0530

    deleted hello component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { TestComponent } from './test/test.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TestComponent ],
+  declarations: [ AppComponent, TestComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
